d91f1714d6766b2a3a9c8efa296f4f60314ec5ad
Danyil <liberlay555@gmail.com>
2024-10-21 18:12:46 +0300

    Add script to auto-close mobile menu on button press

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,23 @@
 const menuOpenBtn = document.querySelector('.mobile-open-btn');
 const menuCloseBtn = document.querySelector('.mobile-menu-close-btn');
 const menuContainer = document.querySelector('.backdrop');
+const mobileMenuBtns = document.querySelectorAll('.mobile-menu-list-link');
+const mobileShopNowBtn = document.querySelector('.menu-shop-now-btn');
 
 menuOpenBtn.addEventListener('click', () => {
   menuContainer.classList.add('is-open');
 });
 
 menuCloseBtn.addEventListener('click', () => {
+  menuContainer.classList.remove('is-open');
+});
+
+mobileMenuBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    menuContainer.classList.remove('is-open');
+  });
+});
+
+mobileShopNowBtn.addEventListener('click', () => {
   menuContainer.classList.remove('is-open');
 });
